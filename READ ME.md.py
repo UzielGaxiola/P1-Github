db2ab3edd5d3e0094b545b044abc6f3fa5f3e6b2
unknown <gaxiolauziel5@gmail.com>
2026-02-03 09:06:14 -0800

    I uploaded a new Read me archive

---
+++ READ ME.md.py
@@ -0,0 +1,21 @@
+# Advance Database Fundamentals
+#
+# This file serves as an introductory README.
+# It explains the purpose of the repository and the objectives of the course.
+#
+# Objective:
+# Learn the  concepts of databases and how to use SQL.
+#
+# Technologies used:
+# - Python
+# - SQL
+# - MySQL
+# - Git
+# - GitHub
+#
+# Author:
+# Gaxiola Elizalde Uziel Hiram
+#
+# Course:
+# Advance Database
+
